523d517b0469ab96b2025e04bd0503d51f9e4d37
anil-appari007 <anil.appari007@gmail.com>
2024-03-05 21:36:25 +0530

    updated Nginx Conf & Dockerfile

--- frontend/src/components/Test.js
+++ frontend/src/components/Test.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, {useEffect, useState} from 'react'
-import { API_URL } from '../config'
+// import { API_URL } from '../config'
+const API_URL = process.env.REACT_APP_API_URL
 const URL = API_URL + "/inventoryList"
 
 const ADD_URL = API_URL + "/addItem"
