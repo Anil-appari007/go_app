48e4215c2103391e0d4e48e1244885dcd29ee547
anil-appari007 <anil.appari007@gmail.com>
2024-03-05 21:57:29 +0530

    Updated API URL

--- frontend/src/components/Test.js
+++ frontend/src/components/Test.js
@@ -7,6 +7,11 @@ const URL = API_URL + "/inventoryList"
 const ADD_URL = API_URL + "/addItem"
 const PUT_URL = API_URL + "/updateItem"
 export default function Table() {
+    if (!API_URL) {
+        console.error('Error: env var REACT_APP_API_URL found to be empty');
+    } else {
+        console.log('Found env REACT_APP_API_URL', API_URL)
+    }
     const [items, setItems] = useState([])
     const [id, setId] = useState('')
     const [name, setName] = useState('')
